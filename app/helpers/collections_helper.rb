64188d6df5dc6d5a192d3f724834589eb2577ade
callilol <rouaux.annesophie@gmail.com>
2013-03-13 16:48:03 +0100

    harvest logs

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,7 +1,23 @@
 module CollectionsHelper
 
-	def last_harvest(collection)
-		harvest = collection.harvest_logs.order('start desc').first	
+	def success(collection)
+		klass = 'warning'
+		klass = collection.last_harvest.success ? 'success' : 'error' if collection.last_harvest
+	end
+
+	def harvest_duration(harvest)
+		Time.at(harvest.end - harvest.start).utc.strftime(' (%H heures et %M minutes) ')
+	end
+
+	def formatted(harvest)
+		if harvest.success
+			display = time_ago_in_words(harvest.start) 
+			display = harvest.full ? t('collection.full') : t('collection.partial')
+			display += harvest_duration(harvest)
+			display += time_ago_in_words(harvest.start)
+		else
+			display = t('collection.failed')
+		end
 	end
 
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,8 @@
 class Collection < ActiveRecord::Base
 	attr_accessible :name, :full_name, :host, :user, :password
 	has_many :harvest_logs
+
+	def last_harvest
+		harvest_logs.order('start desc').first
+	end
 end
